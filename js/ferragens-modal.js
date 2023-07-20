534abb9751a45659077d669387bfc4e91ea60588
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-07-20 11:32:56 -0300

    Ajustes

--- js/ferragens-modal.js
+++ js/ferragens-modal.js
@@ -8,4 +8,15 @@ const ferragensCard1 = () => {
     });
 };
 
+const ferragensCard2 = () => {
+    const imgPequenas = document.querySelectorAll(".js-f-card2-imgP-1")
+    imgPequenas.forEach((img) => {
+        img.addEventListener("click", () => {
+            const imgGrande = document.querySelector(".js-f-card2-imgG-1")
+            imgGrande.src = img.src;
+        });
+    });
+};
+
 ferragensCard1()
+ferragensCard2()

90402873bcbaae495e1403f9b6297927388baa74
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-07-25 15:21:04 -0300

    Ajustes

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,7 +1,14 @@
 const menuMobile = document.querySelector(".js-menu-mobile");
 const navMenu = document.querySelector(".nav__menu");
+const body = document.querySelector(".js-body")
 
 menuMobile.addEventListener("click", () => {
     navMenu.classList.toggle("show");
 });
+
+body.addEventListener("click", () => {
+    if(navMenu.classList.toggle("show")) {
+        return true;
+    }
+})
 

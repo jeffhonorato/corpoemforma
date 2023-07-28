2c742178699eb2264dc1d7c843daa3a48873349d
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-07-28 10:38:02 -0300

    Ajustes

--- js/swiper.ferragens.js
+++ js/swiper.ferragens.js
@@ -1,17 +1,17 @@
-var swiper = new Swiper(".suplementos-box", {
+var swiper = new Swiper(".mySwiper", {
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: "auto",
-    // loop: "true",
     coverflowEffect: {
       rotate: 50,
-      stretch: 0,
-      depth: 100,
+      stretch: 10,
+      depth: 70,
       modifier: 1,
+      slideShadows: false,
     },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
-  });
+});

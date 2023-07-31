82d3e796602a09362a2470a3f57f56a0dfb5481a
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-07-31 08:37:25 -0300

    ajustes

---
+++ js/swiper-fabricacao.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".Myfabricacao", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 10,
+      depth: 70,
+      modifier: 1,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+});

03a32c9df124481c3315cb18e1ae4ef125fc2b54
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-09-18 15:02:29 -0300

    Ajustes

--- js/maquinas.js
+++ js/maquinas.js
@@ -1,4 +1,19 @@
 const itemMenu = document.querySelectorAll(".js-maquinas-item-menu");
 const maquinasConteudo = document.querySelectorAll(".js-conteudo-maquinas")
 
-console.log(maquinasConteudo);
+
+
+//////////// CARDS PEITO ////////////
+const peitoCard1 = () => {
+    const imagensP = document.querySelectorAll(".js-peito-card1-imgP");
+    imagensP.forEach((img) => {
+        img.addEventListener("click", () => {
+            const imagemG = document.querySelector(".js-peito-card1-imgG");
+            imagemG.src = img.src;
+        });
+    });
+}
+
+peitoCard1();
+
+
